cc7ecf433eade820a20568b605f4747ee2d86f5d
jgithub <jgithub1@jeano.net>
2014-11-07 12:39:17 -0600

    Added loosely_false?

--- lib/simple_util_collection/boolean_util.rb
+++ lib/simple_util_collection/boolean_util.rb
@@ -2,7 +2,11 @@ module SimpleUtilCollection
   module BooleanUtilModule
     def loosely_true?(value) 
       ['true', 't', '1', 'yes', 'y'].include?(value.to_s.downcase.strip) rescue false
-    end     
+    end 
+
+    def loosely_false?(value) 
+      ['false', 'f', '0', 'no', 'n'].include?(value.to_s.downcase.strip) rescue false
+    end        
   end
 
   class BooleanUtil
